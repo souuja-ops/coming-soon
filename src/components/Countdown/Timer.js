24bda169d67338197205f8a9c7964558fcdec2f2
souuja-ops.github.io <60695716+souuja-ops@users.noreply.github.com>
2021-06-14 19:36:40 +0300

    Update Timer.js

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -43,7 +43,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("jun 16, 2021 21:00:00").getTime();
+    this.deadline = new Date("jun 20, 2021 21:00:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
